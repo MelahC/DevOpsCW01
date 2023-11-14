9e47a275e6b244f13b66be417f32b67a15624380
MelahC <milacerveira@icloud.com>
2023-11-14 16:08:05 +0000

    added a JUnit4 test for the Dec2Hex class to ensure the correct conversion of decimal numbers to hexadecimal

--- Dec2HexTest.java
+++ Dec2HexTest.java
@@ -6,4 +6,29 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class Dec2HexTest {
+private String capturedOutput;
+
+    @Before
+    public void setUp() {
+        // Redirect System.out.println() to capture the output
+        System.setOut(new java.io.PrintStream(System.out) {
+            public void println(String s) {
+                capturedOutput = s;
+            }
+        });
+    }
+
+    @Test
+    public void testConversionWithValidInput() {
+        String[] args = {"15"};
+        Dec2Hex.main(args);
+        assertEquals("Hexadecimal representation is: F", capturedOutput.trim());
+
+        String actualOutput = capturedOutput.trim();
+        String expectedOutput = "Hexadecimal representation is: F";
+
+        assertTrue(actualOutput.startsWith("Hexadecimal representation is:"));
+        assertTrue(actualOutput.contains("F"));
+        assertEquals(expectedOutput, actualOutput);
+    }
 }
