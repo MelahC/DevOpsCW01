4d2aee8619eb7a230f0b5f70ccb856d8b1e1db5f
MelahC <milacerveira@icloud.com>
2023-11-14 17:25:17 +0000

    received errors compiling using javac -cp .:./junit4.10/junit-4.10.jar *.java corrected errors

--- Dec2HexTest.java
+++ Dec2HexTest.java
@@ -2,14 +2,14 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.fail;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class Dec2HexTest {
-private String capturedOutput;
-//Variable to capture the exit codes 0 & 1 
-private int capturedExitCode;
+    private String capturedOutput;
+    //Variable to capture the exit codes 0 & 1 
+    private int capturedExitCode;
 
     @Before
     public void setUp() {
@@ -37,25 +37,25 @@ private int capturedExitCode;
 
     @Test
     public void testConversionWithNoInputArgument() {
-    String[] args = {};
-    try {
-    Dec2Hex.main(args);
-    fail("Expected System.exit(1) for missing input argument");
-    } catch (SecurityException e) { 
-    assertEquals("System.exit() is not allowed", e.getMessage());
-    assertEquals(1, capturedExitCode);                
-   }
+        String[] args = {};
+        try {
+            Dec2Hex.main(args);
+            fail("Expected System.exit(1) for missing input argument");
+        } catch (SecurityException e) { 
+            assertEquals("System.exit() is not allowed", e.getMessage());
+            assertEquals(1, capturedExitCode);                
+        }
     }
 
     @Test
     public void testConversionWithNonIntegerInput() {
-    String[] args = {"abc"};
-    try {
-    Dec2Hex.main(args);
-    fail("Expected System.exit(0) for non-integer input");
-    } catch (SecurityException e) {
-    assertEquals("System.exit() is not allowed", e.getMessage());
-     assertEquals(0, capturedExitCode);
+        String[] args = {"abc"};
+        try {
+            Dec2Hex.main(args);
+            fail("Expected System.exit(0) for non-integer input");
+        } catch (SecurityException e) {
+            assertEquals("System.exit() is not allowed", e.getMessage());
+            assertEquals(0, capturedExitCode);
+        }
     }
-     }
-
+}
