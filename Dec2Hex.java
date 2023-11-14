41e0030320221927850403bd254bd5f8b79fcb67
MelahC <milacerveira@icloud.com>
2023-11-14 12:34:36 +0000

    added input validation for non interger input. If user input is not a valid interger the programme prints an error message & exits with non error status code gracefully to ensure non interger inputs are not converted

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -14,7 +14,14 @@ class Dec2Hex
        //Exit the program with error code (meaning encountered problem could not complete inteded operation)
         System.exit(1);
      }
+      try {
         Arg1 = Integer.parseInt(args[0]);
+     } catch (NumberFormatException e) {
+      // Handle non intergear input
+       System.out.println("Error: Input not valid. Please enter a  valid decimal number.");
+      //exit code 0 to indicate unable to perform conversion due to invalid user input
+       System.exit(0);
+     }
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
